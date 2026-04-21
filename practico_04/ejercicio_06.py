984eb0c5739023d433b2b61658ac3e9b87117924
valen <valen@DESKTOP-GQP6OPG>
2026-04-21 16:08:30 -0300

    ejercicio_06

--- practico_04/ejercicio_06.py
+++ practico_04/ejercicio_06.py
@@ -1,7 +1,7 @@
 """Base de Datos SQL - Creación de tablas auxiliares"""
 
 from practico_04.ejercicio_01 import borrar_tabla, crear_tabla
-
+import sqlite3
 
 def crear_tabla_peso():
     """Implementar la funcion crear_tabla_peso, que cree una tabla PersonaPeso con:
@@ -9,13 +9,41 @@ def crear_tabla_peso():
         - Fecha: Date()
         - Peso: Int()
     """
-    pass # Completar
+    
+    conexion = sqlite3.connect("tp_python.db")
+    cursor = conexion.cursor()
+    
+    #claves foraneas
+    cursor.execute("PRAGMA foreign_keys = ON")
+    
+    consulta = """
+        CREATE TABLE IF NOT EXISTS PersonaPeso (
+            IdPersona INTEGER,
+            Fecha DATE,
+            Peso INTEGER,
+            FOREIGN KEY (IdPersona) REFERENCES Persona (IdPersona)
+        )
+    """
+    cursor.execute(consulta)
+    conexion.commit()
+    conexion.close()
+    print("Tabla PersonaPeso creada")
+    
 
 
 def borrar_tabla_peso():
     """Implementar la funcion borrar_tabla, que borra la tabla creada 
     anteriormente."""
-    pass # Completar
+    
+    conexion = sqlite3.connect("tp_python.db")
+    cursor = conexion.cursor()
+    
+    cursor.execute("DROP TABLE IF EXISTS PersonaPeso")
+    
+    conexion.commit()
+    conexion.close()
+    print("Tabla PersonaPeso borrada")
+    
 
 
 # NO MODIFICAR - INICIO
@@ -28,3 +56,4 @@ def reset_tabla(func):
         borrar_tabla()
     return func_wrapper
 # NO MODIFICAR - FIN
+
