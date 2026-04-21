36d36e36969693184c7ab3c3d869695db132dbbd
valen <valen@DESKTOP-GQP6OPG>
2026-04-21 15:22:24 -0300

    ejercicio_03

--- practico_04/ejercicio_03.py
+++ practico_04/ejercicio_03.py
@@ -4,13 +4,25 @@ import datetime
 
 from practico_04.ejercicio_01 import reset_tabla
 from practico_04.ejercicio_02 import agregar_persona
-
+import sqlite3
 
 def borrar_persona(id_persona):
     """Implementar la funcion borrar_persona, que elimina un registro en la 
     tabla Persona. Devuelve un booleano en base a si encontro el registro y lo 
     borro o no."""
-    pass # Completar
+    
+    conexion = sqlite3.connect("tp_python.db")
+    cursor = conexion.cursor()
+
+    cursor.execute("DELETE FROM Persona WHERE IdPersona = ?", (id_persona,))
+
+    borrado_exitoso = cursor.rowcount > 0
+    
+    conexion.commit()
+    conexion.close()
+    
+    return borrado_exitoso
+
 
 # NO MODIFICAR - INICIO
 @reset_tabla
