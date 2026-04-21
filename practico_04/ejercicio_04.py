b570d1e7789a4a6beb290b889fe633b7fd95a2cf
valen <valen@DESKTOP-GQP6OPG>
2026-04-21 15:49:07 -0300

    ejercicio_04

--- practico_04/ejercicio_04.py
+++ practico_04/ejercicio_04.py
@@ -4,14 +4,31 @@ import datetime
 
 from practico_04.ejercicio_01 import reset_tabla
 from practico_04.ejercicio_02 import agregar_persona
-
+import sqlite3
 
 def buscar_persona(id_persona):
     """Implementar la funcion buscar_persona, que devuelve el registro de una 
     persona basado en su id. El return es una tupla que contiene sus campos: 
     id, nombre, nacimiento, dni y altura. Si no encuentra ningun registro, 
     devuelve False."""
-    pass # Completar
+    
+    conexion = sqlite3.connect("tp_python.db")
+    cursor = conexion.cursor()
+    
+    consulta = "SELECT * FROM Persona WHERE IdPersona = ?"
+    
+    cursor.execute(consulta, (id_persona,))
+    
+    resultado = cursor.fetchone()
+    
+    conexion.close()
+    
+    if resultado:
+        id, nombre, fecha_str, dni, altura = resultado
+        fecha_obj = datetime.datetime.strptime(fecha_str, '%Y-%m-%d %H:%M:%S')
+        return ( id, nombre, fecha_obj, dni, altura)
+    
+    return False
 
 
 # NO MODIFICAR - INICIO
