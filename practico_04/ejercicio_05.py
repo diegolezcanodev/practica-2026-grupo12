430cfc26abd7ec98fcbec8e46f86b83f29cfefe7
valen <valen@DESKTOP-GQP6OPG>
2026-04-21 15:50:20 -0300

    ejercicio_05

--- practico_04/ejercicio_05.py
+++ practico_04/ejercicio_05.py
@@ -5,13 +5,31 @@ import datetime
 from practico_04.ejercicio_01 import reset_tabla
 from practico_04.ejercicio_02 import agregar_persona
 from practico_04.ejercicio_04 import buscar_persona
-
+import sqlite3
 
 def actualizar_persona(id_persona, nombre, nacimiento, dni, altura):
     """Implementar la funcion actualizar_persona, que actualiza un registro de
     una persona basado en su id. Devuelve un booleano en base a si encontro el
     registro y lo actualizo o no."""
-    pass # Completar
+    
+    conexion = sqlite3.connect("tp_python.db")
+    cursor = conexion.cursor()
+    
+    consulta = """
+        UPDATE Persona
+        SET Nombre = ?, FechaNacimiento = ?, DNI = ?, Altura = ?
+        WHERE IdPersona = ?
+    """
+    cursor.execute(consulta, (nombre, nacimiento, dni, altura, id_persona))
+    
+    actualizado_exitoso = cursor.rowcount > 0
+    
+    conexion.commit()
+    conexion.close()
+    
+    return actualizado_exitoso
+    
+    
 
 # NO MODIFICAR - INICIO
 @reset_tabla
